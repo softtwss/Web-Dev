b543f5fedbf3993be845d518c89318a1de160587
softtwss <157101824+softtwss@users.noreply.github.com>
2025-03-02 20:47:10 +0500

    Add files via upload

--- lab4/src/app/app.component.spec.ts
+++ lab4/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'kaspi' title`, () => {
+  it(`should have the 'kaspi-products' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kaspi');
+    expect(app.title).toEqual('kaspi-products');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kaspi');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kaspi-products');
   });
 });
--- lab4/src/app/app.component.ts
+++ lab4/src/app/app.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './products/products.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/products/header.component';
+import { NgFor } from '@angular/common';
+
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, ProductsComponent],
+  imports: [HeaderComponent, NavBarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'kaspi';
-  scrollToSection(sectionId: string) {
-    document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
-  }
+  title = 'kaspi-products';
 }
 
+
+
+
+// import {Component} from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     Hello {{ city }}, {{ 1 + 1 }}
+//   `,
+// })
+// export class AppComponent {
+//   city = 'San Francisco';
+// }
