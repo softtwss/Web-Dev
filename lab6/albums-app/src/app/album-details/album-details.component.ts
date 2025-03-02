b984b55daf171badeed5b780f480bac5b2bdc548
softtwss <157101824+softtwss@users.noreply.github.com>
2025-03-02 21:41:09 +0500

    Add files via upload

---
+++ lab6/albums-app/src/app/album-details/album-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-details',
+  imports: [],
+  templateUrl: './album-details.component.html',
+  styleUrl: './album-details.component.css'
+})
+export class AlbumDetailsComponent {
+
+}
